ccd5a03635cf8afd7445fc6e7dff0602acf70e71
Mickael Riga <mig@campbellhay.com>
2011-06-20 10:59:18 +0100

    Make the example use Model#backend_delete_form

--- example/basic_admin.rb
+++ example/basic_admin.rb
@@ -21,15 +21,8 @@ BASIC_ADMIN = proc{
       list = eval(model_name).all.map do |m|
         api_inst_path = "%s/%s" % [api_model_path,m.id]
         link = "<a href='%s?_destination=%s'>%s</a>\n" % [api_inst_path,escaped_path,m.to_label]
-        delete_form = <<-EOF
-        <form action='%s' method='POST'>
-        %s
-        <input type='hidden' name='_method' value='DELETE' />
-        <input type='hidden' name='_destination' value='%s' />
-        <input type='submit' name='delete' value='x' />
-        </form>
-        EOF
-        delete_form = delete_form % [api_inst_path,link,escaped_path]
+        delete_form = m.backend_delete_form(api_inst_path, {:destination=>escaped_path})
+        link+delete_form
       end.join
       [200,{'Content-Type'=>'text/html'},[title,create_link,list]]
     }
